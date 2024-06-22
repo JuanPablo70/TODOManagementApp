213e83fc34673ed4209371d4f272706c0b3a8225
Juan Pablo <juanpasan2000@gmail.com>
2024-06-22 15:57:02 -0500

    Header, Footer & Log Out component

--- frontend/todo-app/src/components/todo/TodoApp.jsx
+++ frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -1,20 +1,25 @@
 import { useState } from 'react';
-import { BrowserRouter, Route, Routes, useNavigate, useParams } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes, useNavigate, useParams } from 'react-router-dom';
 import './TodoApp.css';
 
 export default function TodoApp() {
 
     return (
         <div className="TodoApp">
+            
+
             <BrowserRouter>
+                <HeaderComponent />
                 <Routes>
                     <Route path='/' element={ <LoginComponent /> } />
                     <Route path='/login' element={ <LoginComponent /> } />
                     <Route path='/welcome/:username' element={ <WelcomeComponent /> } />
                     <Route path='/todos' element={ <ListTodosComponent /> } />
+                    <Route path='/logout' element={ <LogoutComponent /> } />
 
                     <Route path='*' element={ <ErrorComponent /> } />
                 </Routes>
+                <FooterComponent />
             </BrowserRouter>
         </div>
     );
@@ -67,7 +72,7 @@ function LoginComponent() {
                     <input type="password" name="password" value={password} onChange={handlePasswordChange} />
                 </div>
                 <div>
-                    <button type="button" name="login" onClick={handleSubmit}>Sign in</button>
+                    <button type="button" name="login" onClick={handleSubmit}>Log in</button>
                 </div>
             </div>
         </div>
@@ -84,7 +89,7 @@ function WelcomeComponent() {
         <div className="WelcomeComponent">
             <h1>Welcome {params.username}</h1>
             <div>
-                Welcome Component
+                Manage your todos - <Link to="/todos">Go here</Link>
             </div>
         </div>
     );
@@ -105,21 +110,27 @@ function ErrorComponent() {
 
 function ListTodosComponent() {
 
+    const today = new Date();
+
+    const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay());
+
     const todos = [
-                    {id:1, description: 'Learn Angular'},
-                    {id:2, description: 'Learn Full stack dev'},
-                    {id:3, description: 'Learn MongoDB'}
+                    {id:1, description: 'Learn Angular', done: false, targetDate: targetDate},
+                    {id:2, description: 'Learn Full stack dev', done: false, targetDate: targetDate},
+                    {id:3, description: 'Learn MongoDB', done: false, targetDate: targetDate}
                 ];
 
     return (
-        <div className="ListTodosComponent">
+        <div className="container">
             <h1>What you want to do!</h1>
             <div>
-                <table>
+                <table className="table">
                     <thead>
                         <tr>
-                            <td>id</td>
-                            <td>description</td>
+                            <td>Id</td>
+                            <td>Description</td>
+                            <td>is done?</td>
+                            <td>Target Date</td>
                         </tr>
                     </thead>
                     <tbody>
@@ -129,6 +140,8 @@ function ListTodosComponent() {
                                     <tr key={todo.id}>
                                         <td>{todo.id}</td>
                                         <td>{todo.description}</td>
+                                        <td>{todo.done.toString()}</td>
+                                        <td>{todo.targetDate.toDateString()}</td>
                                     </tr>
                                 )
                             )
@@ -136,6 +149,57 @@ function ListTodosComponent() {
                     </tbody>
                 </table>
             </div>
+        </div>
+    );
+
+}
+
+
+function HeaderComponent() {
+
+    return (
+        <header className="border-bottom border-light border-5 mb-5 p-2">
+        <div className="container">
+            <div className="row">
+                <nav className="navbar navbar-expand-lg">
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav">
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/welcome/admin">Home</Link></li>
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/todos">Todos</Link></li>
+                        </ul>
+                    </div>
+                    <ul className="navbar-nav">
+                        <li className="nav-item fs-5"><Link className="nav-link" to="/login">Login</Link></li>
+                        <li className="nav-item fs-5"><Link className="nav-link" to="/logout">Logout</Link></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    );
+
+}
+
+function FooterComponent() {
+
+    return (
+        <footer className="FooterComponent">
+            <div className="container">
+                Develop this project with <a className="navbar-brand" href="https://www.udemy.com/course/spring-boot-and-spring-framework-tutorial-for-beginners/">in28minutes!</a>
+            </div>
+        </footer>
+    );
+
+}
+
+
+function LogoutComponent() {
+
+    return (
+        <div className='LogoutComponent'>
+            <h1>You just Log Out</h1>
+            <div>Thank you for user our app!</div>
         </div>
     );
 
