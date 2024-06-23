98432200157aafdd379c10536d2ce8c82f50e499
Juan Pablo <juanpasan2000@gmail.com>
2024-06-23 17:01:46 -0500

    Requests for REAT API enabled from localhost:3000

--- backend/todoapi/src/main/java/com/spring/rest/todoapi/TodoapiApplication.java
+++ backend/todoapi/src/main/java/com/spring/rest/todoapi/TodoapiApplication.java
@@ -2,12 +2,30 @@ package com.spring.rest.todoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TodoapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoapiApplication.class, args);
+	}
+
+	/**
+	 * Add a CORS mapping overriding addCorsMappings method
+	 * @return
+	 */
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
