ae2367af6b588fd2858c979312b94b48a46693b4
Juan Pablo <juanpasan2000@gmail.com>
2024-06-23 17:44:21 -0500

    Todos displayed from Spring REST API

--- frontend/todo-app/src/components/todo/ListTodosComponent.jsx
+++ frontend/todo-app/src/components/todo/ListTodosComponent.jsx
@@ -1,14 +1,20 @@
+import { useEffect, useState } from "react";
+import { retrieveAllTodosByUser } from "./service/TodoApiService";
+
 export default function ListTodosComponent() {
 
-    const today = new Date();
+    const [todos, setTodos] = useState([]);
 
-    const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay());
+    useEffect ( () => refreshTodos(), [] );
 
-    const todos = [
-                    {id:1, description: 'Learn Angular', done: false, targetDate: targetDate},
-                    {id:2, description: 'Learn Full stack dev', done: false, targetDate: targetDate},
-                    {id:3, description: 'Learn MongoDB', done: false, targetDate: targetDate}
-                ];
+    function refreshTodos() {
+        retrieveAllTodosByUser('admin')
+            .then(response => {
+                setTodos(response.data)
+                }
+            )
+            .catch(error => console.log('error :>> ', error));
+    }
 
     return (
         <div className="container">
@@ -31,7 +37,7 @@ export default function ListTodosComponent() {
                                         <td>{todo.id}</td>
                                         <td>{todo.description}</td>
                                         <td>{todo.done.toString()}</td>
-                                        <td>{todo.targetDate.toDateString()}</td>
+                                        <td>{todo.targetDate.toString()}</td>
                                     </tr>
                                 )
                             )
