17150e178ee10d798c3126736dcb04978eabf4d0
Juan Pablo <juanpasan2000@gmail.com>
2024-06-28 20:43:10 -0500

    Basic authentication

---
+++ backend/todoapi/src/main/java/com/spring/rest/todoapi/security/BasicAuthenticationSecurityConfiguration.java
@@ -0,0 +1,34 @@
+package com.spring.rest.todoapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BasicAuthenticationSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        //Authenticate all requests
+        httpSecurity.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+
+        //Basic authentication
+        httpSecurity.httpBasic(Customizer.withDefaults());
+
+        //Stateless rest api
+        httpSecurity.sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        //Disabling csrf
+        httpSecurity.csrf().disable();
+        
+        return httpSecurity.build();
+    }
+
+}
